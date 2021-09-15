6bba905baf63aee8a5a54e1e930664ea6445f552
Lucas <lucasdsveras@gmail.com>
2021-09-15 00:20:13 -0300

    Initial commit

---
+++ code/03_analyse.R
@@ -0,0 +1,102 @@
+# Load packages and functions ---------------------------------------------
+
+library(here)
+library(tidyverse)
+library(psych)
+library(car)
+source(here("code", "funs.R"))
+
+# Load data ---------------------------------------------------------------
+
+load(here("data", "data.rda"))
+
+# Principal component analysis --------------------------------------------
+
+# Build a correlation matrix
+corr_matrix <- data |>
+  select(where(is.double), - c(bmc, fm, alst)) |>
+  cor()
+# Compute its determinant
+corr_matrix_det <- det(corr_matrix)
+# Barlett test
+barlett <- cortest.bartlett(corr_matrix, n = nrow(data))
+# Kaiser-Meyer-Olkin test
+kmo <- kmo(corr_matrix)
+
+# 1st PCA model (extracting the same number of variables as factors)
+pc1 <- principal(
+  corr_matrix, nfactors = length(corr_matrix[1, ]), rotate = "varimax"
+)
+# Scree plot
+plot(pc1$values, type = "b")
+
+# 2nd PCA model (extracting 6 factors, as suggested by the eigenvalues and
+# the scree plot)
+pc2 <- principal(corr_matrix, nfactors = 6, rotate = "varimax")
+# Print factors with a loading > 0.5
+print.psych(pc2, cut = 0.5, sort = TRUE)
+
+# Univariate models -------------------------------------------------------
+
+bmc_uni <- lm(
+  bmc ~ sf_axillary + sf_suprailiac + circ_abdominal + sf_subscapular +
+    sf_biceps + sf_abdominal + circ_waist + circ_hip + circ_thorax +
+    circ_arm + weight + br_biiliac + height + half_armspan + knee_height +
+    br_biacromial + br_bimalleolar + br_chest + br_wrist + br_cubital +
+    circ_forearm + br_knee + circ_calf + sf_thigh + circ_thigh + sf_triceps +
+    age + sf_chest,
+  data = data
+)
+bmc_uni_step <- step(bmc_uni)
+summary(bmc_uni_step)
+
+fm_uni <- lm(
+  fm ~ sf_axillary + sf_suprailiac + circ_abdominal + sf_subscapular +
+    sf_biceps + sf_abdominal + circ_waist + circ_hip + circ_thorax +
+    circ_arm + weight + br_biiliac + height + half_armspan + knee_height +
+    br_biacromial + br_bimalleolar + br_chest + br_wrist + br_cubital +
+    circ_forearm + br_knee + circ_calf + sf_thigh + circ_thigh + sf_triceps +
+    age + sf_chest,
+  data = data
+)
+fm_uni_step <- step(fm_uni)
+summary(fm_uni_step)
+
+alst_uni <- lm(
+  alst ~ sf_axillary + sf_suprailiac + circ_abdominal + sf_subscapular +
+    sf_biceps + sf_abdominal + circ_waist + circ_hip + circ_thorax +
+    circ_arm + weight + br_biiliac + height + half_armspan + knee_height +
+    br_biacromial + br_bimalleolar + br_chest + br_wrist + br_cubital +
+    circ_forearm + br_knee + circ_calf + sf_thigh + circ_thigh + sf_triceps +
+    age + sf_chest,
+  data = data
+)
+alst_uni_step <- step(alst_uni)
+summary(alst_uni_step)
+
+# Multivariate model ------------------------------------------------------
+
+# Set a matrix of the outcomes
+y <- cbind(data$alst, data$fm, data$bmc)
+colnames(y) <-  c("alst", "fm", "bmc")
+# Build the model
+m <- lm(
+  y ~ weight + half_armspan + br_cubital + sf_triceps + sf_thigh + sf_chest,
+  data = data
+)
+
+# Get model statistics
+coeffs <- map(summary(m), "coefficients") |>
+  map(~ .x[, 1]) |>
+  map(~ t(t(.x))) |>
+  map(round, 5)
+r2 <- map(summary(m), "r.squared") |> map(round, 2)
+adj_r2 <- map(summary(m), "adj.r.squared") |> map(round, 2)
+resid_SE <- map(summary(m), "sigma") |> map(round, 2)
+# Pillai's trace
+Manova(m)
+
+# PRESS cross-validation
+press_m <- press(m) |> round(2)
+q2_m <- q2(m) |> round(2)
+se_m <- se_press(m) |> round(2)
